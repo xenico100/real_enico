cd4c92f546afd09bf9ef2f4dc0b7f3afcc97d090
Enico <enicoveck@Enicoui-Macmini.local>
2026-09-24 22:16:03 +0900

    feat: add blueprint pants front model shot to 1st thumbnail

--- scripts/register_cinema_products.mjs
+++ scripts/register_cinema_products.mjs
@@ -48,6 +48,7 @@ export const CINEMA_PRODUCTS = [
     is_published: true,
     specs: `${R2_BASE}/%EC%83%81%EC%84%B8%ED%8E%98%EC%9D%B4%EC%A7%80/detail_blueprint_pants.jpg`,
     images: [
+      `${R2_BASE}/%EC%8D%B8%EB%84%A4%EC%9D%BC/blueprint_pants_00_front.jpg`,
       `${R2_BASE}/%EC%8D%B8%EB%84%A4%EC%9D%BC/blueprint_pants_01.jpg`,
       `${R2_BASE}/%EC%8D%B8%EB%84%A4%EC%9D%BC/blueprint_pants_02.jpg`,
       `${R2_BASE}/%EC%8D%B8%EB%84%A4%EC%9D%BC/blueprint_pants_03.jpg`,
